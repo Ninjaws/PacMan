43a5ced0ea24f5e1215d7577bf726783ef704389
Whisper <gewoonjvr@gmail.com>
2018-05-30 13:51:47 +0200

    [Fixed] launcher presentations
    see summary

--- PacManV2/src/application/launcher/presentation/panes/LauncherPane.java
+++ PacManV2/src/application/launcher/presentation/panes/LauncherPane.java
@@ -17,7 +17,7 @@ public class LauncherPane extends BorderPane {
     private LauncherPane() {
         instance = this;
 
-        setId("application.launcher-pane");
+        setId("launcher-pane");
 
         this.setTop(new TopPane());
         this.setCenter(new LoginPane());
